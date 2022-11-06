f94600c7137928b1df57c1a313ca3c94b581cfbe
Mohitb07 <bmohit980@gmail.com>
2022-11-06 22:14:06 +0530

    hero section implemented initial commit

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: ['i.pinimg.com', "www.mast-jaegermeister.de"],
+  },
 }
 
 module.exports = nextConfig
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      screens: {
+        '3xl': '1800px',
+        '4xl': '2000px',
+      },
+    },
+  },
+  plugins: [],
+}
